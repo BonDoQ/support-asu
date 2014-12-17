0a4ac964e481cd041ce1c720184d8d14266f1d4f
keroishak <kero.ishak@gmail.com>
2014-12-17 20:34:33 +0200

    support

--- app/views/Cpanel/events.blade.php
+++ app/views/Cpanel/events.blade.php
@@ -11,7 +11,7 @@
         <div class="col-sm-12">
         <div class="panel panel-primary filterable">
             <div class="panel-heading">
-                <h3 class="panel-title">Users</h3>
+                <h3 class="panel-title">Events</h3>
                 <div class="pull-right">
                     <button class="btn btn-default btn-xs btn-filter"><span class="glyphicon glyphicon-filter"></span> Filter</button>
                 </div>
--- app/views/Cpanel/sponsers.blade.php
+++ app/views/Cpanel/sponsers.blade.php
@@ -12,7 +12,7 @@
 	<div class="row">
         <div class="panel panel-primary filterable">
             <div class="panel-heading">
-                <h3 class="panel-title">Users</h3>
+                <h3 class="panel-title">Sponsors</h3>
                 <div class="pull-right">
                     <button class="btn btn-default btn-xs btn-filter"><span class="glyphicon glyphicon-filter"></span> Filter</button>
                 </div>
