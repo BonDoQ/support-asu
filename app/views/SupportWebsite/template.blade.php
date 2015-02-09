cc3e40bdb066a166434a5b1acb0a979e7917ada2
keroishak <kero.ishak@gmail.com>
2015-02-09 03:08:05 +0200

    support

--- app/views/SupportWebsite/template.blade.php
+++ app/views/SupportWebsite/template.blade.php
@@ -83,11 +83,11 @@
               <li>
                 <a href="#myModal" data-toggle="modal">Contact us</a>
               </li>
-              <!--li {{{ (Request::is('registration') ? 'class=active' : '') }}}>
+              <li {{{ (Request::is('registration') ? 'class=active' : '') }}}>
                 <a href="{{{ URL::to('/registration')  }}}">
                   <span style="color:#f8b619;">Registration</span>
                 </a>
-              </li-->
+              </li>
             </ul>
           </div><!--/.nav-collapse -->
         </div><!-- /.navbar-inner -->
